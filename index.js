a8901e66904f53a786bebca9ded18125b9b564aa
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-09-22 12:16:43 -0400

    commit tree 804006261336ec24ffa60b2495a72bc0e35aef85

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(function(name) {
+        return name.toUpperCase() === string.toUpperCase();
+    });
+}
+function fuzzyMatch(array, string) {
+    return array.filter(function(name) {
+        return name.substring(0, 2) === string.substring(0, 2);
+    });
+}
+function matchName(driver, string) {
+    return driver.filter(function(object) {
+        return object.name === string;
+    });
+}
